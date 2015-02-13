70778cc6195f0773cbbb5f9b79563df8a347ede2
Brad Morse <bkmorse@gmail.com>
2015-02-13 15:56:35 -0500

    added travis yml

--- tests/BaseballTest.php
+++ tests/BaseballTest.php
@@ -6,10 +6,10 @@ class BaseballTest extends \PHPUnit_Framework_TestCase {
 
 	private $baseball;
 
-    public function __construct()
-    {
-        $this->baseball = new Baseball();
-    }
+	public function __construct()
+	{
+	    $this->baseball = new Baseball();
+	}
 
 	public function testBattingAverage()
 	{
